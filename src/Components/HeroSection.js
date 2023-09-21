86a1fafb78c54de6d01103b32bef92edd2de624c
unknown <ashishkumarnath9@gmail.com>
2023-09-21 23:55:04 +0530

    updated Portfolio

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import './HeroSection.css'
-import '../App.css';
+import React from "react";
+import "./HeroSection.css";
+import "../App.css";
 
 function HeroSection() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-3.mp4' autoPlay loop muted />
-      <h1>Hello!, I am Ashish Kumar Nath</h1>
-      <p>Looking to utilize my technology and programming skills in an esteemed organization. Well versed with HTML/CSS ,JavaScript,ReactJs and GIT. Tech-savvy and a fast learner with  natural curiosity for web development .</p>
+    <div className="hero-container">
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <h1>Hello!,I'm Ashish 👋 </h1>
+      <p>
+        I'm a frontend developer with 1.7 years of experience, including a stint
+        at Tech Mahindra where I collaborated with renowned clients. My
+        expertise lies in Angular 14, along with proficiency in HTML, CSS,
+        ReactJS, Git, and Node.js. I'm passionate about crafting seamless user
+        experiences and pushing the boundaries of frontend development.
+      </p>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
