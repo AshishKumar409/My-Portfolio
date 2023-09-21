c0949338863dba92f6dca6c38c19c33f5ac7e2d0
unknown <ashishkumarnath9@gmail.com>
2023-09-21 23:56:53 +0530

    updated Portfolio

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import './Cards.css';
-import Carditem from './Carditem';
+import React from "react";
+import "./Cards.css";
+import Carditem from "./Carditem";
 
 function Cards() {
   return (
-    <div className='cards'>
-      <h1>My Projects</h1>
-      <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
+    <div className="cards">
+      <h1>My Personal Projects</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
             <Carditem
-              src='images/pizza.png'
-              text=' Landing page'
+              src="images/pizza.png"
+              text=" Landing page"
               alt="'Pizza Landing page'"
-              label='Landing page'
-              path='https://pizza-web9.netlify.app/'
+              label="Landing page"
+              path="https://pizza-web9.netlify.app/"
             />
             <Carditem
-              src='images/login.png'
-              text='Login form'
-              label='Login form'
-              path='https://client-side-form-validation9.netlify.app/'
+              src="images/login.png"
+              text="Login form"
+              label="Login form"
+              path="https://client-side-form-validation9.netlify.app/"
             />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <Carditem
-              src='images/recipe.png'
-              text='Recipe app'
-              label='Recipe app'
-              path='https://ashishkumar409.github.io/recipe-app/'
+              src="images/recipe.png"
+              text="Recipe app"
+              label="Recipe app"
+              path="https://ashishkumar409.github.io/recipe-app/"
             />
             <Carditem
-              src='images/quiz.png'
-              text='Quiz app'
-              label='Quiz app'
-              path='https://ashishkumar409.github.io/Quiz/'
+              src="images/quiz.png"
+              text="Quiz app"
+              label="Quiz app"
+              path="https://ashishkumar409.github.io/Quiz/"
             />
           </ul>
         </div>
